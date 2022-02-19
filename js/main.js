1a093dbdb0d66b651463d490ffa1a19152be45dd
MuxammadAli7707 <muxammadali7707@gmail.com>
2022-02-19 23:55:48 +0500

    initial commit

--- js/main.js
+++ js/main.js
@@ -14,3 +14,23 @@ elClosemodal.addEventListener('click', function () {
   elClosemodal.classList.remove('show-it');
 });
 
+var topbutton = document.getElementById('up');
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    topbutton.style.opacity = "1";
+    topbutton.style.transition = "all 0.5s linner";
+  } else {
+    topbutton.style.opacity = "0";
+    topbutton.style.transition = "all 0.5s linner";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
